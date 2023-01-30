15bb9e8002ec97cffb054b256916ecab29072927
Kylie-Lang1 <kylielang@pursuit.org>
2023-01-30 12:14:42 -0500

    added NavBar and css

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,12 @@
-function NavBar(props) {
+import { Link } from "react-router-dom"
+import "../css/NavBar.css"
+
+function NavBar() {
     return (
         <nav>
-            <h1>Snack-A-Log</h1>
+            <Link to="/"><h1>Snack-A-Log</h1></Link>
+            <Link to="/snacks/new"><h2>Add Snack</h2></Link>
+            <Link to="/snacks"><h2>All Snacks</h2></Link>
         </nav>
     );
 }
