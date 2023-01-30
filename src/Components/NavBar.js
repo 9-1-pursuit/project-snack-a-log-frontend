a6652a4dc39b074542215b9e62cbf03ac617061f
Richie-Pursuit <richiecarrasco@pursuit.org>
2023-01-30 12:13:25 -0500

    test branch

---
+++ src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>Snack-A-LOG</h1>
+            
+        </nav>
+    );
+};
+
+export default NavBar;
