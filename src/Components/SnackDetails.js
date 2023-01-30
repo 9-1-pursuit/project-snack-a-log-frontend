cc7c8aefc625ecbf4730c47360565586d052e178
Gillesedgy <edgygilles@pursuit.org>
2023-01-30 14:07:13 -0500

    Show Page Nedds More Styling.. Added buttons but not styles

--- src/Components/SnackDetails.js
+++ src/Components/SnackDetails.js
@@ -31,53 +31,46 @@ export default function SnackDetails() {
   };
 
   return (
-    // <div className="card">
-    //   <div className="card_content">
-    //     {/* //! FRONT */}
-    //     <div className="card_front">
-    //       {" "}
-    //       <h3 className="card_title">{snack.name}</h3>
-    //       <img className="card_subtitle" src="" alt="Snack_Icon" />
-    //     </div>
-
-    //     {/* //! BACK */}
-    //     <div className="card_back">
-    //       {" "}
-    //       {/* //TODO: SMALLER IMAGE FOR SNACK  */}
-    //       <div className="card_body">
-    //         <img src={placeholder} alt="Snack_Icon" />
-    //         <p>Fiber: {snack.fiber}</p>
-    //         <p>Protein: {snack.protein}</p>
-    //         <p>Sugar: {snack.added_sugar}</p>
-    //         <p>HEALTHY?: {snack.is_healthy}</p>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-    <div className="card">
-      <div className="card__content">
-        <div className="card__front">
-          <h3 className="card__title">{snack.name}</h3>
-          <img
-            className="card__subtitle"
-            src="https://picsum.photos/seed/first/1920/1080"
-            alt="snack_icon"
-          />
-        </div>
-
-        <div className="card__back">
-          <div className="card__body">
-            <img className="body_image"
-              src="https://picsum.photos/seed/first/1920/1080"
+    <>
+      <div className="card">
+        <div className="card__content">
+          <div className="card__front">
+            <h3 className="card__title">{snack.name}</h3>
+            <img
+              className="card__subtitle"
+              src={snack.image}
               alt="snack_icon"
             />
-            <p>Fiber: {snack.fiber}</p>
-            <p>Protein: {snack.protein}</p>
-            <p>Sugar: {snack.added_sugar}</p>
-            <p>HEALTHY?: {snack.is_healthy}</p>
+          </div>
+
+          <div className="card__back">
+            <div className="card__body">
+              <img className="body_image" src={snack.image} alt="snack_icon" />
+              <p>Fiber: {snack.fiber}</p>
+              <p>Protein: {snack.protein}</p>
+              <p>Sugar: {snack.added_sugar}</p>
+              <p>HEALTHY?: {snack.is_healthy}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="button_container">
+        <button
+          onClick={() => {
+            navigate(`/snacks`);
+          }}
+        >
+          Back!
+        </button>
+        <button
+          onClick={() => {
+            navigate(`/snacks/${id}/edit`);
+          }}
+        >
+          Edit
+        </button>
+        <button onClick={deleteSnack}>Delete</button>
+      </div>
+    </>
   );
 }
