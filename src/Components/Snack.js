5d840a932a7d430dbb8b0177e105fdd48d8c5b3c
Fadila-Ali <fadilaali@pursuit.org>
2023-01-30 12:28:06 -0500

    Links added

--- src/Components/Snack.js
+++ src/Components/Snack.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import regular from "../assets/heart-regular.png"
-import solid from "../assets/heart-solid.png"
-export default function Snack({ snack}) {
-
+import React from "react";
+import { Link } from "react-router-dom";
+import regular from "../assets/heart-regular.png";
+import solid from "../assets/heart-solid.png";
+export default function Snack({ snack }) {
   return (
     <section>
-      <img src={snack.image} alt="Snack image"></img>
-      <h5>{snack.name}</h5>
+      <Link to={`/snacks/${snack.id}`}>
+        <img src={snack.image} alt="Snack image"></img>
+      </Link>
+      <h5>
+        <Link to={`/snacks/${snack.id}`}>{snack.name}</Link>
+      </h5>
       {snack.is_healthy ? (
         <span>
           <img src={solid}></img>
