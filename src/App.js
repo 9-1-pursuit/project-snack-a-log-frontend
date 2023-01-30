b61ec73e8cfaf353b5d30380087cdd9abfa0467d
NRiddick113 <NatashaRiddick@pursuit.org>
2023-01-30 15:29:51 -0500

    Basic routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+// DEPENDENCIES
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// PAGES
+import Edit from "./Pages/Edit";
+import FourOFour from "./Pages/FourOFour.js";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import New from "./Pages/New";
+import Show from "./Pages/Show";
+
+// COMPONENTS
+import NavBar from "./Components/NavBar";
+
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <Router>
+        <NavBar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/snacks" element={<Index />} />
+          <Route path="/snacks/new" element={<New />} />
+          <Route exact path="/snacks/:id" element={<Show />} />
+          <Route path="/snacks/:id/edit" element={<Edit />} />
+          <Route path="*" element={<FourOFour />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
