9bbd84f546e4f220fa1e623df7e2228c3d0c9f34
Richie-Pursuit <richiecarrasco@pursuit.org>
2023-01-31 01:54:10 -0500

    added routes & pages files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Edit from "./pages/Edit";
+import FourOFour from "./pages/FourOFour";
+import Home from "./pages/Home";
+import Show from "./pages/Show";
+import NavBar from "./Components/NavBar";
+import New from "./pages/New";
+import Index from "./pages/Index";
+
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/snacks" element={<Index/>} />
+          <Route path="/snacks/new" element={<New />} />
+          <Route path="/snacks/:id" element={<Show />} />
+          <Route path="/snacks/:id/edit" element={<Edit />} />
+          <Route path="/error" elememt={<FourOFour />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Show.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Show = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Show;
