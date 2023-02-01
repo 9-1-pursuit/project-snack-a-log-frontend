7e31424e1450354f4ffea112341fdb235d2d1820
Salinamalek <salinamalek@pursuit.org>
2023-01-31 19:45:52 -0500

    show page base done

--- src/Components/SnackShow.js
+++ src/Components/SnackShow.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
+import SolidHeart from "../assets/heart-solid.png";
+import EmptyHeart from "../assets/heart-regular.png";
 import "./SnackShow.css";
 
 export default function SnackShow() {
@@ -30,27 +32,67 @@ export default function SnackShow() {
 
   return (
     <>
-      <article>
-        <h3>{snack.name}</h3>
-        <h5>Image: {snack.image}</h5>
-        <h5>Description: {snack.description}</h5>
-        <h5>Fiber: {snack.fiber}</h5>
-        <h5>Protein: {snack.protein}</h5>
-        <h5>Carbs: {snack.carbs}</h5>
-        <h5>Added Sugar: {snack.added_sugar}</h5>
-        <h5>Healthy: {snack.is_healthy}</h5>
-        <div>
-          <Link to={`/snacks`}>
-            <button>Back</button>
-          </Link>
+      <article className="snack-info">
+        <h1>{snack.name}</h1>
+        <div className="snackInfo">
+          <img className="snack-img" src={snack.image} alt="snack-image" />
+          <div className="snack-text">
+            <h5>
+              <strong>
+                <b>Description: </b>
+              </strong>
+              {snack.description}
+            </h5>
+            <h5>
+              <strong>
+                <b>Fiber: </b>
+              </strong>
+              {snack.fiber} grams
+            </h5>
+            <h5>
+              <strong>
+                <b>Protein: </b>
+              </strong>
+              {snack.protein} grams
+            </h5>
+            <h5>
+              <strong>
+                <b>Carbs: </b>
+              </strong>
+              {snack.carbs} grams
+            </h5>
+            <h5>
+              <strong>
+                <b>Added Sugar: </b>
+              </strong>
+              {snack.added_sugar} grams
+            </h5>
+            <h5>
+              <strong>
+                <b>Healthy: </b>
+              </strong>
+              {snack.is_healthy ? (
+                <img src={SolidHeart} alt="logo" height="25px" />
+              ) : (
+                <img src={EmptyHeart} alt="logo" height="25px" />
+              )}
+            </h5>
+          </div>
         </div>
-        <div>
-          <Link to={`/snacks/${id}/edit`}>
-            <button>Edit</button>
-          </Link>
-        </div>
-        <div>
-          <button onClick={handleDelete}>Delete</button>
+        <div className="buttons">
+          <div>
+            <Link to={`/snacks`}>
+              <button>Back</button>
+            </Link>
+          </div>
+          <div>
+            <Link to={`/snacks/${id}/edit`}>
+              <button>Edit</button>
+            </Link>
+          </div>
+          <div>
+            <button onClick={handleDelete}>Delete</button>
+          </div>
         </div>
       </article>
     </>
