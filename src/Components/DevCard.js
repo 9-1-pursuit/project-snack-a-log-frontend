b8f788bea384e63e3c0b1fe69dd21326509666f5
Salinamalek <salinamalek@pursuit.org>
2023-02-01 11:16:53 -0500

    commented out bootstrap

--- src/Components/DevCard.js
+++ src/Components/DevCard.js
@@ -1,38 +1,34 @@
-import React from 'react';
-import { Button } from 'react-bootstrap'
-import "./DevCard.css"
+import React from "react";
+// import { Button } from 'react-bootstrap'
+import "./DevCard.css";
 
-const DevCard = ({dev}) => {
-    const {
-        imageURL,
-        name,
-        githubURL,
-        indeedURL,
-        email,
-        description
-    } = dev
+const DevCard = ({ dev }) => {
+  const { imageURL, name, githubURL, indeedURL, email, description } = dev;
 
-    return (
-        <article>
-        <img
-        src={imageURL}></img>
-        <div className='dev-content'>
-            <h3>{name}</h3>
-            <p>{description}</p>
-        </div>
-        <div className='dev-buttons'>
-            <a href={githubURL}>
-                <button variant="primary">Github</button>
-            </a>
-            <a href={indeedURL}>
-                <button href={indeedURL} variant="primary">Indeed</button>
-            </a>
-            <a href={email}>
-                <button href={email} variant="primary">Email</button>
-            </a>
-        </div>
-        </article>
-    );
+  return (
+    <article>
+      <img src={imageURL}></img>
+      <div className="dev-content">
+        <h3>{name}</h3>
+        <p>{description}</p>
+      </div>
+      <div className="dev-buttons">
+        <a href={githubURL}>
+          <button variant="primary">Github</button>
+        </a>
+        <a href={indeedURL}>
+          <button href={indeedURL} variant="primary">
+            Indeed
+          </button>
+        </a>
+        <a href={email}>
+          <button href={email} variant="primary">
+            Email
+          </button>
+        </a>
+      </div>
+    </article>
+  );
 };
 
 export default DevCard;
