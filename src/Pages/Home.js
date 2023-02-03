8e73f3bbb8bf1780ef9a56acf286d4e5b7a8c014
DestinyJoyner <destinyjoyner@pursuit.org>
2023-02-03 00:27:19 -0500

    added home page summary minor css

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,15 +7,14 @@ export default function Home() {
       {/* <h1>Welcome to Snax!</h1> */}
       <img src={welcome} alt="welcome" />
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Augue eget arcu
-        dictum varius duis at consectetur lorem donec.{" "}
+        <h4>Welcome to SNAX!</h4>
+        An app that allows you to log your favorite snacks, and some of ours too 😉 .{" "} While traversing this app, we have ensured that you will be able to see if whether or{" "}
         <Link id="hint" to="/snacks/new">
           d3v$f4v$
-        </Link>{" "}
-        Velit laoreet id donec ultrices tincidunt. Elementum facilisis leo vel
-        fringilla est ullamcorper eget. At in tellus integer feugiat scelerisque
-        varius morbi enim. Volutpat commodo sed egestas egestas fringilla.
+        </Link>{" "} not your snack is healthy.
+        View some of our favorite couch and blanket companions, add your own snacks, come back to update them, and if they have lived on past their expiration date, delete them. 
+        <br/>
+        Click on any of the above links to get started!  
       </p>
       {/* <img src="https://media0.giphy.com/media/WQZJkXIPyxsFW4wuaC/giphy.gif?cid=790b76115edec05a6fac57a8cd9d14772b2b9a7ee9004857&rid=giphy.gif&ct=s" alt="snack all day" height="340px" /> */}
     </div>
